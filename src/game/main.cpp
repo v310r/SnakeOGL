5270f9cf524a6e9da940830069a409a44089173c
v310r <webmail200601@gmail.com>
2021-12-25 19:09:54 +0200

    the beginning

---
+++ src/game/main.cpp
@@ -0,0 +1,15 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "../image_loader/stb_image.h"
+
+#include <iostream>
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
